abbc63b995b245564d65ba45661f626469f7bd78
Sheyla <Sheylapopovich@gmail.com>
2022-02-06 13:22:21 -0700

    imported apolloClient inMemoryCache and createHttpLink from apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Route, Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
         <h1>Application</h1>
-        
+
         <Router>
-        <>
-          <Switch>
-            <Route path="/dashboard" component={Dashboard} />
-            
-        
-          </Switch>
+          <>
+            <Switch>
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
           </>
         </Router>
       </div>
