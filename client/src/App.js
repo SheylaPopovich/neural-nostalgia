9b197746b14945cef31d56cff87dad5b8e41549f
Sheyla <Sheylapopovich@gmail.com>
2022-02-06 14:02:13 -0700

    added route exact path to login component succesfully

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,27 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import LoginForm from "../src/pages/LoginForm";
-
+import LoginForm from "./pages/LoginForm";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
 
 function App() {
   return (
@@ -27,7 +35,7 @@ function App() {
       <Router>
         <>
           <Routes>
-            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/" element={<LoginForm />} />
           </Routes>
         </>
       </Router>
