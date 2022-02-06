8a48664b0d0356728fea395af97c6210413b1d38
Sheyla <Sheylapopovich@gmail.com>
2022-02-06 12:58:57 -0700

    rendered all components onto app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import Dashboard from "../src/components/Dashboard/Dashboard";
 import Preferences from "../src/components/Preferences/Preferences";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
