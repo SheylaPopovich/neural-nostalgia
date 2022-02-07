254ee0f85ef227988d0aa170208ad657156acff4
Sheyla <Sheylapopovich@gmail.com>
2022-02-06 19:47:18 -0700

    rendered exact path to profile component

--- client/src/PrivateRoutes.js
+++ client/src/PrivateRoutes.js
@@ -3,3 +3,16 @@ import { Route, Redirect } from "react-router-dom";
 import Auth from "./utils/auth";
 
 import Profile from "./pages/Profile";
+
+ 
+ export default function PrivateRoutes() {
+    if (Auth.loggedIn()) {
+   return (
+   <>
+   <Route exact path="/" component={Profile} />
+   
+   </>
+   );
+ }
+}
+ 
