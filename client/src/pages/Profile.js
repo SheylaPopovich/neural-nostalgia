4af9714cb1b052fae4ffd1ca4f50fa2dff215858
Sheyla <Sheylapopovich@gmail.com>
2022-02-06 19:31:57 -0700

    copied over error message const for profile js file

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,2 +1,19 @@
 import React, { useState } from "react";
 import { useMutation, useQuery } from "@apollo/client";
+import Auth from "../utils/auth";
+import { GET_ME } from "../utils/queries";
+
+
+export default function Profile() {
+    const [errorMessage, setErrorMessage] = useState("");
+
+
+
+
+
+
+
+
+
+  return <div></div>;
+}
