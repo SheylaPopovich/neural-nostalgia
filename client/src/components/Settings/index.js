9ee6de0a36ceef1601aad02574dea954f17c4d7a
Sheyla <Sheylapopovich@gmail.com>
2022-02-08 19:34:47 -0700

    opened export default function names settings

--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -2,3 +2,16 @@ import React, { useState } from "react";
 import { useMutation, useQuery } from "@apollo/client";
 
 import { GET_ALL_USERS } from "../../utils/queries";
+
+
+export default function Settings({ users, projectId }) {
+
+      // Form handlers
+  const [newUser, setNewUser] = useState({ label: "", id: "" });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Gets all the user
+  const { data } = useQuery(GET_ALL_USERS);
+  const userData = data?.users || [];
+
+  
