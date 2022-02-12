6c9aaed1c819d71e7abe7691d9c2dd068116280e
Sheyla <Sheylapopovich@gmail.com>
2022-02-12 16:51:01 -0700

    inserted app use to server js file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
-
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
@@ -12,7 +11,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
